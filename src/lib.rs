f6e7876588de3378c68309fd960aa64e14d27d0c
That3Percent <That3Percent@gmail.com>
2019-07-05 15:48:46 -0500

    Added try_js_intern

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@
 //! * [soa-vec](https://github.com/That3Percent/soa-vec) A struct of arrays layout with a Vec of tuple API
 //! * [second-stack](https://github.com/That3Percent/second-stack) A memory allocator for large slices that don't escape the stack.
 pub use js_intern_core::js_intern;
+pub use js_intern_proc_macro::try_js_intern;
 
 #[cfg(test)]
 mod tests {
@@ -57,4 +58,15 @@ mod tests {
 	fn deduplicates_bool() {
 		assert_eq!(js_intern!(true) as *const _, js_intern!(true) as *const _);
 	}
+
+	// TODO: It would be nice to have tests around try_js_intern, but that would require enabling the proc_macro_hygiene feature,
+	// but I'm not sure what effect that would have on crates which would rely only on js_intern and not try_js_intern if they
+	// would also need to upgrade to nightly
+	/*
+	#[wasm_bindgen_test]
+	fn try_deduplicates_str_lit() {
+		assert_eq!(try_js_intern!("a") as *const _, js_intern!("a") as *const _);
+	}
+	*/
+
 }
