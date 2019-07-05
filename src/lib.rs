c70b5735b5047106eb9a3af5093edb2db5e279d6
That3Percent <That3Percent@gmail.com>
2019-07-05 15:47:53 -0500

    Updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 //! Stores one copy of each distinct JavaScript primitive.
-//! For example, ```js_intern!("string")``` evaluates to a ```&JsValue``` but only
-//! does the translation from the utf-8 Rust string to the utf-16 JavaScript
-//! string the first time the expression is evaluated. Furthermore, strings
-//! are de-duplicated across the program. So, any time ```js_intern!("string")```
-//! is used in the program, the same instance of the JavaScript string is used.
+//! For example, ```js_intern!("string")``` evaluates to a ```&JsValue``` but uses only one heap allocation
+//! and a one-time translation from the utf-8 Rust string to the utf-16 JavaScript string the first time the expression is evaluated.
+//! Furthermore, strings are de-duplicated across the program.
+//! So, any time ```js_intern!(1.0)``` is used in the program, the same instance of the JavaScript number is used.
 //!
 //! # Supported types
 //! * ```&'static str``` Eg: ```js_intern!("str")```
@@ -11,9 +10,10 @@
 //! * ```bool``` Eg: ```js_intern(true)```
 //!
 //! # Related
-//! If you like this, you may like these other crates by Zac Burns
+//! If you like this, you may like these other crates by Zac Burns (That3Percent)
+//! * [js-object](https://github.com/That3Percent/js-object) A macro for creating JavaScript objects
 //! * [soa-vec](https://github.com/That3Percent/soa-vec) A struct of arrays layout with a Vec of tuple API
-
+//! * [second-stack](https://github.com/That3Percent/second-stack) A memory allocator for large slices that don't escape the stack.
 pub use js_intern_core::js_intern;
 
 #[cfg(test)]
